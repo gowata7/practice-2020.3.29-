2a9750ebf0c0e1401703311a77f0acca8e8a3f5c
gowata7 <62722852+gowata7@users.noreply.github.com>
2020-04-03 23:20:05 +0900

    Create __init__.py

---
+++ animal/__init__.py
@@ -0,0 +1,5 @@
+from .cat import Cat # "이 폴더에 있는" cat.py라는 파일에서 Cat이라는 클래스를 갖고와줘
+from .dog import Dog # "현재 폴더에 있는" dog.py라는 파일에서 Dog라는 클래스를 갖고와줘
+
+# from .cat import * : cat.py 파일에서 모든 코드를 불러와줘
+# from .dog import * : dog.py 파일에서 모든 코드를 불러와줘
