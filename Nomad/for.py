a073303d8dbb96b0e4b15c91c6be853329566a07
gowata7 <62722852+gowata7@users.noreply.github.com>
2020-04-11 03:50:05 +0900

    for문

---
+++ Nomad/for.py
@@ -0,0 +1,10 @@
+days = ("mon","tue","wed","thr","fri","sat","sun")
+
+for day in days:
+  if day == "wed":
+    break # "wed"가 나오면 loop를 빠져나간다
+  else:
+    print(day)
+
+for seq in "SEQUENCE": # 문자열도 배열이다.
+  print(seq)
